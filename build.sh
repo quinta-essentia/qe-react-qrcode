00498448c0d0f621e119a0f1c9ad42407fa17658
Petar Basic <petar@qe-macmini-02.local>
2020-10-29 15:04:48 +0100

    Publishing fix

--- build.sh
+++ build.sh
@@ -4,14 +4,5 @@ rm -r dist
 mkdir dist
 cp package.public.json dist/package.json
 
-./node_modules/.bin/babel ./elements --out-dir dist/elements --extensions ".js,.jsx" --env-name prod
-./node_modules/.bin/babel ./form --out-dir dist/form --extensions ".js,.jsx" --env-name prod
-./node_modules/.bin/babel ./services --out-dir dist/services --extensions ".js,.jsx" --env-name prod
-./node_modules/.bin/babel ./utilities --out-dir dist/utilities --extensions ".js,.jsx" --env-name prod
+./node_modules/.bin/babel ./component/blocks --out-dir dist/elements --extensions ".js,.jsx" --env-name prod
 ./node_modules/.bin/babel ./index.js --out-file dist/index.js --env-name prod
-
-./node_modules/.bin/copyfiles "./elements/**/*.png" dist
-./node_modules/.bin/copyfiles "./form/**/*.png" dist
-
-./node_modules/.bin/copyfiles "./elements//**/*.d.ts" dist
-./node_modules/.bin/copyfiles "./form/**/*.d.ts" dist
