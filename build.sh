e8034b0d40099f9fb4e2ad96bc439684b8ee3ce1
basic <petar.basic@tradecore.com>
2020-10-30 13:34:33 +0100

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ rm -r dist
 mkdir dist
 cp package.public.json dist/package.json
 
-./node_modules/.bin/babel ./src/components/blocks --out-dir dist --extensions ".js,.jsx" --env-name prod
+./node_modules/.bin/babel ./components/blocks --out-dir dist/components/blocks --extensions ".js,.jsx" --env-name prod
 ./node_modules/.bin/babel ./index.js --out-file dist/index.js --env-name prod
