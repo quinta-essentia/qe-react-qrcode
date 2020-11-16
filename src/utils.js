3708794ba27fe4eb84b17318df3e44fe24410900
Petar Basic <petar@qe-macmini-02.local>
2020-11-16 17:32:20 +0100

    Removing unnecessary coments

--- src/utils.js
+++ src/utils.js
@@ -56,14 +56,10 @@ const downloadAsPng = (id) => {
 const imageDimensions = file =>
   new Promise((resolve, reject) => {
     const img = new Image();
-
-    // the following handler will fire after the successful loading of the image
     img.onload = () => {
       const { naturalWidth: width, naturalHeight: height } = img;
       resolve({ width, height });
     };
-
-    // and this handler will fire if there was an error with the image (like if it's not really an image or a corrupted one)
     img.onerror = (err) => {
       reject(new Error('There was some problem with the image.', err));
     };
